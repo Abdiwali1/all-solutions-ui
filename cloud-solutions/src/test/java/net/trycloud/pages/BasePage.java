0a9f2cc06e25ae0191a267cad7051b0adfd9384e
AdamB25 <barsialtunit@gmail.com>
2024-02-06 11:57:34 -0500

    projects are done

--- cloud-solutions/src/test/java/net/trycloud/pages/BasePage.java
+++ cloud-solutions/src/test/java/net/trycloud/pages/BasePage.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class BasePage {
+public abstract class BasePage {
 
     @FindBy(xpath="//div[@class='avatardiv avatardiv-shown']")
     public WebElement avatarIcon;
